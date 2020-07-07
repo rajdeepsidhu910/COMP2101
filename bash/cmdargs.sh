d179805f03758c78322ef2ddcd33ddbb9335153f
rajdeepsidhu910 <66766072+rajdeepsidhu910@users.noreply.github.com>
2020-07-07 19:42:23 -0400

    lab 4

--- bash/cmdargs.sh
+++ bash/cmdargs.sh
@@ -3,11 +3,6 @@
 
 # create an empty array to put the command line arguments into
 myargs=()
-vcheck=0
-dcheck=0
-#It will create variables to recognize the stat of the input.
-
-echo "Myarg array contents: $@"
 # loop through the command line arguments
 while [ $# -gt 0 ]; do
   # tell the user how many things are left on the command line
@@ -17,72 +12,25 @@ while [ $# -gt 0 ]; do
   # tell the user what we did
   echo "Added \'$1\' to the arguments array"
 
-  # TASK 1: instead of just adding arguments to an array, use a case statement to recognize some options
-  echo "Processing the value '$1'."
-  # Options to recognize: -h for help, -v for verbose mode, -d N for debug mode with N being a single digit number
-  case $1 in
-  -h )
-	# If the help option is recognized, print out help for the command and exit
-    echo 'You added "-h" for help.'
-    #echo 'Processing the "-h".'
-    echo "Choose -v for Varbose, -d for Debug and debug level within 1 to 5."
-	exit
-    ;;
-  -v )
-	# If the verbose option is recognized, set a variable to indicate verbose mode is on
-    echo 'You added "-v" for varbose.'
-    vcheck=1
-    ;;
-  -d )
-	# If the debug optionis recognized, set a variable with the debug level from the number given after the -d on the command line
-    echo 'You added "-d" for debug.'
-    case $2 in
-    [1-5] )
-		echo "You added the -d for debug level $2."
-		dcheck=$2
-		shift
-		;;
-    *)
-		# display an error if the user gave the -d option without a single digit number after it
-		echo "Error: The debug mode should be between [1-5]. "
-		esac
-		;;
-  *)
-  errors=$1
-  echo "Error: unkown value found: $errors"
-  # Anything that wasn't recognized on the command line should still go into the myargs array
-  #  shift
-  ;;
-  esac
+# TASK 1: instead of just adding arguments to an array, use a case statement to recognize some options
+#          Options to recognize: -h for help, -v for verbose mode, -d N for debug mode with N being a single digit number
+#          If the help option is recognized, print out help for the command and exit
+#          If the verbose option is recognized, set a variable to indicate verbose mode is on
+#          If the debug optionis recognized, set a variable with the debug level from the number given after the -d on the command line
+#             display an error if the user gave the -d option without a single digit number after it
+#          Anything that wasn't recognized on the command line should still go into the myargs array
+
   # each time through the loop, shift the arguments left
   # this decrements the argument count for us
   shift
-  #It will shift  the command line argument.
-
   # tell the user we shifted things
   echo "Shifted command line, leaving $# things left to process."
   echo "--------------------------"
   # go back to the top of the loop to see if anything is left to work on
 done
 echo "Done"
+
 # TASK2: display the settings and myargs contents
-# Tell the user if vebose mode is on
-# Print out the myargs array with a label
-#If condition will check the stat of input
-
-if [ $vcheck = 1 ]; then
-  echo "Varbose mode is On."
-else
-  echo "Varbose mode is Off."
-fi
-# TASK2: display the settings and myargs contents
-# Tell the user if debug mode is on and if it is, what number it is set to
-# Print out the myargs array with a label
-#If condition will check the stat of input
-
-if [ $dcheck -gt 0 ]; then
-  echo "Debug Mode is On with debug level $dcheck."
-else
-  echo "Debug Mode is Off."
-fi
-
+#         Tell the user if vebose mode is on
+#         Tell the user if debug mode is on and if it is, what number it is set to
+#         Print out the myargs array with a label
--- bash/network-config-expanded.sh
+++ bash/network-config-expanded.sh
@@ -61,37 +61,27 @@ EOF
 # Per-interface report
 #####
 # define the interface being summarized
-#interface="eno1"
+interface="eno1"
 
 # Find an address and hostname for the interface being summarized
 # we are assuming there is only one IPV4 address assigned to this interface
+ipv4_address=$(ip a s $interface|awk -F '[/ ]+' '/inet /{print $3}')
+ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
 
 # Identify the network number for this interface and its name if it has one
-totalinterface=$(lshw -class network | awk '/logical name:/{print $3}' | wc -l)
-#It will create a loop check the work in the array.
-for((w=1;w<=$totalinterface;w+=1));
-do
-  interface=$(lshw -class network |awk '/logical name:/{print $3}' |awk -v z=$w 'NR==z{print $1; exit}')
-  if [[ $interface = lo* ]] ; then continue ; fi
-  #It will skip loopback interface
-  ipv4_address=$(ip a s $interface | awk -F '[/ ]+' '/inet /{print $3}')
-  ipv4_hostname=$(getent hosts $ipv4_address | awk '{print $2}')
-  network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
-  network_number=$(cut -d / -f 1 <<<"$network_address")
-  network_name=$(getent networks $network_number|awk '{print $1}')
-  #It will turn the output od the commands into the variable.
+network_address=$(ip route list dev $interface scope link|cut -d ' ' -f 1)
+network_number=$(cut -d / -f 1 <<<"$network_address")
+network_name=$(getent networks $network_number|awk '{print $1}')
 
-  echo Interface $interface:
-  echo ===============
-  echo Address         : $ipv4_address
-  echo Name            : $ipv4_hostname
-  echo Network Address : $network_address
-  echo Network Name    : $network_name
-  #Here, echo will print everything.
+cat <<EOF
+Interface $interface:
+===============
+Address         : $ipv4_address
+Name            : $ipv4_hostname
+Network Address : $network_address
+Network Name    : $network_name
 
-done
-#This command will end the loop.
-
+EOF
 #####
 # End of per-interface report
 #####
